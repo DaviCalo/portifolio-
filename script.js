478fbdc7cd797bcb38dad91658a38e780fab13b4
Davi C. Pes. <davicalo2d@gmail.com>
2024-05-05 23:49:53 -0300

    style-adjust_scroll

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('linkaboutme').classList.add('hover');
   });
 
-var mains = document.getElementById('main').addEventListener('scroll', () => {
-        var scrollVertical = document.getElementById('main').scrollTop;
+var aboutmenav = document.getElementById('aboutmenav');
+var aboutmesectionnav = document.getElementById('linkaboutme');
 
-        var aboutmenav = document.getElementById('aboutmenav');
-        var aboutmesectionnav = document.getElementById('linkaboutme');
-
-var projectsectionTop = (document.getElementById('articleproject')).offsetTop; 
 var projectnav = document.getElementById('projectnav');
 var projectsectionnav = document.getElementById('linkproject');
 
-var contatsectionTop = (document.getElementById('articlecontat')).offsetTop; 
 var contatnav = document.getElementById('contatnav');
 
 const icons = {
@@ -24,21 +19,27 @@ const icons = {
     aboutmesectionnav: aboutmesectionnav.classList
 };
 
-        const removeHover = (icon) => icons[icon].remove('hover');
-        const addHover = (icon) => icons[icon].add('hover');
+const removeHover = (icon) => icons[icon].remove('hover');
+const addHover = (icon) => icons[icon].add('hover');
 
-        if (scrollVertical+50 >= projectsectionTop && scrollVertical <= contatsectionTop) {
-            //GAMBIARRA
-            ['aboutmenav', 'contatnav','aboutmesectionnav'].forEach(removeHover);
-            ['projectnav', 'projectsectionnav'].forEach(addHover);
-        } else if(scrollVertical <= projectsectionTop){
-            ['projectnav', 'contatnav','projectsectionnav'].forEach(removeHover);
-            ['aboutmenav', 'aboutmesectionnav'].forEach(addHover);
-        } else {
-            ['projectnav', 'aboutmenav','aboutmesectionnav'].forEach(removeHover);
+var mains = document.getElementById('main').addEventListener('scroll', () => {
+    var scrollVertical = document.getElementById('main').scrollTop;
 
-            addHover('contatnav');
-        }
+    var projectsectionTop = (document.getElementById('articleproject')).offsetTop; 
+
+    var contatsectionTop = (document.getElementById('articlecontat')).offsetTop; 
+
+    if (scrollVertical+50 >= projectsectionTop && scrollVertical <= contatsectionTop) {
+        //GAMBIARRA
+        ['aboutmenav', 'contatnav','aboutmesectionnav'].forEach(removeHover);
+        ['projectnav', 'projectsectionnav'].forEach(addHover);
+    } else if(scrollVertical <= projectsectionTop){
+        ['projectnav', 'contatnav','projectsectionnav'].forEach(removeHover);
+        ['aboutmenav', 'aboutmesectionnav'].forEach(addHover);
+    } else {
+        ['projectnav', 'aboutmenav','aboutmesectionnav'].forEach(removeHover);
+        addHover('contatnav');
+    }
       /** 
           if (scrollVertical >= projectsectionTop && scrollVertical <= contatsectionTop) {
             projecticon.classList.add('hover');
@@ -56,19 +57,4 @@ const icons = {
             aboutmeicon.classList.remove('hover');
         }
         */
-    // Get a reference to the scrollTo function
-    
     })
-/*
-    const { scrollTo } = document.getElementById('main');
-
-    // Override the scrollTo function
-    document.getElementById('main').scrollTo = (...args) => {
-        // Create a new scrollTo function that scrolls smoothly
-        const [x = 0, y = 0] = args;
-        document.getElementById('main').scroll({
-            top: y,
-            left: x,
-            behavior: 'smooth'
-        });
-    };*/
